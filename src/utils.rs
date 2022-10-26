56098d0e5c89c89bd30631256dcde02b6484d745
dmytro-sh <dmytro.sh@blaize.tech>
2022-10-26 18:05:31 +0300

    utils

--- src/utils.rs
+++ src/utils.rs
@@ -1,23 +1,45 @@
+use near_sdk::BorshStorageKey;
 use crate::*;
-use near_sdk::ext_contract;
+use near_sdk::serde::{Deserialize, Serialize};
+use near_sdk::borsh::BorshSerialize;
 
-pub const NO_DEPOSIT: Balance = 0;
 
-#[ext_contract(ext_token)]
-pub trait NEP141Token {
-    fn ft_transfer_call(
-        &mut self,
-        receiver_id: AccountId,
-        amount: WBalance,
-        memo: Option<String>,
-        msg: String,
-    );
+pub type PoolId = String;
+// param pool_id: a string like token_a|token_b|fee
 
-    fn ft_transfer(&mut self, receiver_id: AccountId, amount: WBalance, memo: Option<String>);
+pub type LptId = String;
+// param lpt_id: a string like pool_id|inner_id
+
+
+/// Single action. Allows to execute sequence of various actions initiated by an account.
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "near_sdk::serde")]
+#[serde(untagged)]
+pub enum Action {
+    Swap(SwapAction),
 }
 
-#[ext_contract(ext_market)]
-pub trait MarketInterface {
-    fn borrow(&mut self, amount: WBalance) -> PromiseOrValue<U128>;
-    fn view_market_data(&self) -> MarketData;
+
+/// Single swap action.
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "near_sdk::serde")]
+pub struct SwapAction {
+    /// Pool which should be used for swapping.
+    pub pool_id: u64,
+    /// Token to swap from.
+    pub token_in: AccountId,
+    /// Amount to exchange.
+    /// If amount_in is None, it will take amount_out from previous step.
+    /// Will fail if amount_in is None on the first step.
+    pub amount_in: Option<U128>,
+    /// Token to swap into.
+    pub token_out: AccountId,
+    /// Required minimum amount of token_out.
+    pub min_amount_out: U128,
+}
+
+
+#[derive(BorshSerialize, BorshStorageKey)]
+pub enum StorageKeys {
+    TestFieldStorageKey
 }
